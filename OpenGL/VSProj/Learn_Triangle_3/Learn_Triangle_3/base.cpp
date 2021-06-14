998fbdf495f623b07396b056442cf46531fe2117
RandyWaler <867924722@qq.com>
2021-06-14 21:58:00 +0800

    null

--- OpenGL/VSProj/Learn_Triangle_3/Learn_Triangle_3/base.cpp
+++ OpenGL/VSProj/Learn_Triangle_3/Learn_Triangle_3/base.cpp
@@ -1,11 +1,10 @@
-#include <glad.h> //用于统一化的 进行驱动版本的查询 获取OpenGL的函数指针
-#include <glfw3.h> //用于统一化的 在不同平台上，进行OpenGL窗口的创建，响应按键等一些基本的操作
+#include <glad.h>
+#include <glfw3.h>
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height); //响应windows 动态调整窗口大小的函数
 
 void processInput(GLFWwindow* window, int key, int scancode, int action, int mode);//输入回调事件
-
 
 int main()
 {
@@ -34,13 +33,55 @@ int main()
         return -1;
     }
 
-    //设置渲染视口大小
-    glViewport(0, 0, 800, 600);//与GLFW窗口等大，也可以更小，可以将一些其它元素显示在OpenGL视口之外
+    //设置视口大小
+    glViewport(0, 0, 800, 600);
 
     //注册回调函数
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     glfwSetKeyCallback(window, processInput);
+
+
+
+    //Triangle
+
+
+    //定义一个顶点数据(Vertex Data),包含三个顶点(Vertex)
+    //每个顶点只有位置信息
+    //坐标位于 规范化设备坐标(NDC)下
+
+    // 模型空间 ---》 世界空间 ---》观察空间 ---》 齐次裁剪空间 ---》 NDC ---》 屏幕空间 
+
+    float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+    };
+
+    //创建一个顶点缓冲对象(Vertex Buffer Objects, VBO) 用于管理显存
+    unsigned int VBO; 
+    glGenBuffers(1, &VBO); //创建一个对象缓冲
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);//将这个对象缓冲绑定为 GL_ARRAY_BUFFER （顶点缓存对象类型）
+
+    //将顶点数据复制到缓冲的内存中
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    //缓存内存类型，数据尺寸，数据对象，发送模式
+
+    //GL_STATIC_DRAW ：数据不会或几乎不会改变
+    //GL_DYNAMIC_DRAW：数据会被改变很多 (确保显卡把数据放在能够高速写入的内存部分)
+    //GL_STREAM_DRAW ：数据每次绘制时都会改变 (确保显卡把数据放在能够高速写入的内存部分)
+
+
+
+
+
+
+
+
+
+
 
     //启动渲染
     while (!glfwWindowShouldClose(window)) //每次检查是否要求窗口退出
@@ -50,7 +91,7 @@ int main()
 
         //渲染事件
 
-        glClearColor(0.2f, 0.2f, 0.3f, 1.0f); //设置清屏颜色（状态设置）
+        glClearColor(0.2f, 0.2f, 0.3f, 1.0f); //设置清屏颜色
         glClear(GL_COLOR_BUFFER_BIT); //清空后置颜色缓冲区 --- 填充为glClearColor设置的清屏颜色
 
         glfwSwapBuffers(window); //双缓冲呈递
