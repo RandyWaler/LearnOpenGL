192710d853bc1c1dd28953ea992dad999837ad2c
RandyWaler <867924722@qq.com>
2021-06-19 23:39:05 +0800

    null

--- OpenGL/VSProj/Learn_Triangle_3/Learn_Triangle_3/base.cpp
+++ OpenGL/VSProj/Learn_Triangle_3/Learn_Triangle_3/base.cpp
@@ -132,9 +132,7 @@ int main()
     else std::cout << "SHADER_PROGRAM_LINK_SUCCESS" << std::endl;
 
 
-    //链接合成后，着色器可以被删除
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    
     
 
 
@@ -158,14 +156,14 @@ int main()
     // 模型空间 ---》 世界空间 ---》观察空间 ---》 齐次裁剪空间 ---》 NDC ---》 屏幕空间 
 
     float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
+    -0.7f, -0.5f, 0.5f,
+     -0.3f, -0.5f, 0.5f,
+     -0.5f, 0.0f, 0.5f
     };
 
     //创建一个顶点缓冲对象(Vertex Buffer Objects, VBO) 用于管理显存
     unsigned int VBO;
-    glGenBuffers(1, &VBO); //创建一个对象缓冲 ，第一个参数 ID 是独一无二的，并且从1开始
+    glGenBuffers(1, &VBO); //创建一个对象缓冲 ，生成对象的数量，绑定到的 uint 对象ID
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);//将这个对象缓冲绑定为 GL_ARRAY_BUFFER （顶点缓存对象类型）
 
@@ -178,9 +176,6 @@ int main()
     //GL_DYNAMIC_DRAW：数据会被改变很多 (确保显卡把数据放在能够高速写入的内存部分)
     //GL_STREAM_DRAW ：数据每次绘制时都会改变 (确保显卡把数据放在能够高速写入的内存部分)
 
-
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
     //指定顶点数据的解析方式&启用顶点属性（配置输入顶点属性）#############################################################
 
@@ -201,6 +196,128 @@ int main()
 
 
 
+    //创建另一个黄色，线框长方形的绘制@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+    //固定输出黄色的片段元着色器
+
+    const char* fragmentShader_yellowSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "FragColor = vec4(1.0f, 0.85f, 0.0f, 1.0f);\n"
+        "}\n";
+
+    unsigned int fragmentShader_yellow;
+    fragmentShader_yellow = glCreateShader(GL_FRAGMENT_SHADER);
+
+
+    glShaderSource(fragmentShader_yellow, 1, &fragmentShader_yellowSource, NULL);
+    glCompileShader(fragmentShader_yellow);
+
+    glGetShaderiv(fragmentShader_yellow, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader_yellow, 512, NULL, infoLog);
+        std::cout << "\n---------------------------------------------" << std::endl;
+        std::cout << "【ERROR】 FRAGMENT_SHADER_YELLOW_COMPILATION_FAILED\n" << infoLog << std::endl;
+        std::cout << "\n---------------------------------------------" << std::endl;
+    }
+    else std::cout << "FRAGMENT_SHADER_YELLOW_COMPILATION_SUCCESS" << std::endl;
+
+
+    //着色程序
+    unsigned int shaderProgram_yellow;
+    shaderProgram_yellow = glCreateProgram();
+
+    glAttachShader(shaderProgram_yellow, vertexShader);
+    glAttachShader(shaderProgram_yellow, fragmentShader_yellow);
+    glLinkProgram(shaderProgram_yellow);
+
+    //检测是否链接成功
+    glGetProgramiv(shaderProgram_yellow, GL_LINK_STATUS, &success);
+
+
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram_yellow, 512, NULL, infoLog);
+        std::cout << "\n---------------------------------------------" << std::endl;
+        std::cout << "【ERROR】 SHADER_PROGRAM_YELLOW_LINK_FAILED\n" << infoLog << std::endl;
+        std::cout << "\n---------------------------------------------" << std::endl;
+
+    }
+    else std::cout << "SHADER_PROGRAM_YELLOW_LINK_SUCCESS" << std::endl;
+
+
+    //VAO
+
+    unsigned int VAO2; //创建
+    glGenVertexArrays(1, &VAO2);
+
+    glBindVertexArray(VAO2);//绑定
+
+
+    //顶点&索引
+
+    float vertices2[] = {
+        0.8f,0.8f,0.5f, //右上角
+        0.8f,0.1f,0.5f, //右下角
+        0.1f,0.1f,0.5f, //左下角
+        0.1f,0.8f,0.5f, //左上角
+    };
+
+    unsigned int indices[] = { // 三角带 一逆一顺 为正面
+        2,1,3,0
+    };
+
+    //unsigned int indices[] = { // 先顺后逆，反面，会被背面剔除掉
+    //   0,1,3,2
+    //};
+
+    /*
+    
+       3--------0
+       |        |
+       |        |
+       |        |
+       2--------1
+    
+    */
+
+    //VBO 保存顶点数据
+    unsigned int VBO2;
+    glGenBuffers(1, &VBO2);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO2); //绑定缓冲对象，录入数据
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+
+    //EBO
+
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);//绑定缓冲对象，录入数据
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    //配置顶点属性
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+
+
+    //启动渲染前
+
+    //链接合成后，着色器可以被删除
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    glDeleteShader(fragmentShader_yellow);
+
+    //开启背面剔除
+    glCullFace(GL_BACK); //设置剔除面（默认是背面）
+    glEnable(GL_CULL_FACE); //激活剔除
+
+    //GL_BACK 剔除背面
+    //GL_FRONT 剔除正面
+    //GL_FRONT_AND_BACK 正面背面都剔除
 
     //启动渲染
     while (!glfwWindowShouldClose(window)) //每次检查是否要求窗口退出
@@ -216,6 +333,7 @@ int main()
 
         //渲染对象
         
+        //渲染三角型
         glUseProgram(shaderProgram); //指定渲染程序(材质)
         glBindVertexArray(VAO);//绑定VAO
         glDrawArrays(GL_TRIANGLES, 0, 3);//渲染三角型
@@ -225,9 +343,43 @@ int main()
         //第三个参数：绘制三个顶点
 
 
+        //GL_TRIANGLES 单个三角型(逆时针卷绕为正面)
+        
+        //GL_TRIANGLE_STRIP 三角带 一逆一顺为正面的模式
+
+        //GL_TRIANGLE_FAN 三角扇，每两点与始心点逆时针卷入为正
+
+
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); //绘制为线框
+
+
+
+        //渲染矩形
+        glUseProgram(shaderProgram_yellow);
+        glBindVertexArray(VAO2);
+        glDrawElements(GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_INT, 0); //三角带只需要4个顶点
+
+        //第一个参数：图元类型
+        //第二个参数：绘制6个顶点
+        //第三个参数：数据类型 (EBO 是 uint 索引)
+        //第四个参数：EBO数组的起始索引
+
+       glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);//取消绘制为线框
+
+
         glfwSwapBuffers(window); //双缓冲呈递
         
     }
+
+    //删除缓冲对象
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteVertexArrays(1, &VAO2);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &VBO2);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
+    glDeleteProgram(shaderProgram_yellow);
 
     glfwTerminate();//退出时释放资源
 
